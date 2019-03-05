a6c1e7113cf3fe81f3ec969948ef84b4eac669b5
JuanCalderon <Pest1lozzi>
2019-03-05 08:24:29 -0700

    send data

--- app/controllers/justificaciones_controller.rb
+++ app/controllers/justificaciones_controller.rb
@@ -31,13 +31,11 @@ class JustificacionesController  < ApplicationController
       end
       format.pdf do
 
-=begin
         filename = "#{@justificacion.autorizo.cuenta_cimav}_#{@justificacion.requisicion}.pdf"
         pdf = Prawn::Document.new
         pdf.text "Hello There"
         send_data pdf.render,  type: 'application/pdf', disposition: "inline"
-=end
-
+=begin
         filename = "#{@justificacion.autorizo.cuenta_cimav}_#{@justificacion.requisicion}.pdf"
         # if @justificacion.tipo.fraccion == 1 ||  @justificacion.tipo.fraccion == 7
         if @justificacion.tipo.fraccion == 7
@@ -51,7 +49,7 @@ class JustificacionesController  < ApplicationController
                   type: 'application/pdf',
                   disposition: "inline"
         File.delete("public/#{filename}")
-
+=end
       end
     end
   end
