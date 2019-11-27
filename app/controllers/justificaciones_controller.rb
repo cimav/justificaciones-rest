12df201fe41529e065a00c8a1a0054dba94f96c4
JuanCalderon <juan.calderon@gmail.com>
2019-11-27 10:34:09 -0700

    Justificaciones desde 1 año antes hasta 1 año después

--- app/controllers/justificaciones_controller.rb
+++ app/controllers/justificaciones_controller.rb
@@ -15,9 +15,9 @@ class JustificacionesController  < ApplicationController
     ids = Asistente.where("asistente_id = #{params[:id]}").collect(&:creador_id).concat(Array(params[:id])).join(',')
     # @justificaciones = Justificacion.where("creador_id = #{params[:id]}")
     if ids.include? "999"
-      @justificaciones = Justificacion.includes(:creador).select("id, identificador, requisicion, creador_id, created_at, descripcion").where(created_at: 1.year.ago..Time.now)
+      @justificaciones = Justificacion.includes(:creador).select("id, identificador, requisicion, creador_id, created_at, descripcion").where(created_at: 1.year.ago..1.year.after)
     else
-      @justificaciones = Justificacion.includes(:creador).select("id, identificador, requisicion, creador_id, created_at, descripcion").where("creador_id in (#{ids})").where(created_at: 1.year.ago..Time.now) #.order(:requisicion)
+      @justificaciones = Justificacion.includes(:creador).select("id, identificador, requisicion, creador_id, created_at, descripcion").where("creador_id in (#{ids})").where(created_at: 1.year.ago..1.year.after) #.order(:requisicion)
     end
     render json: @justificaciones, methods: [:creador_cuenta_cimav]
 
