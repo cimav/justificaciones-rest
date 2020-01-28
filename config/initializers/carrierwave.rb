b93a1bded9052e554f81617a82259c75f5d716eb
JuanCalderon <juan.calderon@gmail.com>
2020-01-28 15:55:04 -0700

    Justificaciones desde 1 año antes hasta 1 año después

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,11 @@
 CarrierWave.configure do |config|
-  config.permissions = 0766           # drwxrw-rw-
-  config.directory_permissions = 0766 # -rwxrw-rw-
-  config.storage = :file
-
-#  config.permissions = 0777           # drwxrw-rw-
-#  config.directory_permissions = 0777 # -rwxrw-rw-
+#  config.permissions = 0766           # drwxrw-rw-
+#  config.directory_permissions = 0766 # -rwxrw-rw-
 #  config.storage = :file
+
+  config.permissions = 0777           # drwxrw-rw-
+  config.directory_permissions = 0777 # -rwxrw-rw-
+  config.storage = :file
 
   # This avoids uploaded files from saving to public/ and so
   # they will not be available for public (non-authenticated) downloading
